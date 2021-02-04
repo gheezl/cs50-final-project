3d4baa7f779875bd062f294ab51ead174eb02b0a
jacob Rushlow <gheezl25@gmail.com>
2021-02-04 10:35:17 -0500

    Building the base of the application

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+
+const App = () => {
+  return(
+    <Fragment>
+      <div>
+        <span>Hello</span>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App;
