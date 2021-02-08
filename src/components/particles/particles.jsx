574108f8df9c4d31677f7c5bb86b4a6a7f90917d
jacob Rushlow <gheezl25@gmail.com>
2021-02-08 13:08:24 -0500

    Added more to the particles page

--- src/components/particles/particles.jsx
+++ src/components/particles/particles.jsx
@@ -20,7 +20,7 @@ const Background = () => {
                     "line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},
                     "move":{"enable":true,"speed":1,"direction":"right","random":true,"straight":true,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
                     "interactivity":{"detect_on":"window","events":{"onhover":{"enable":true,"mode":"none"},
-                    "onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
+                    "onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
                     "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},
                     "repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true})
                 }
